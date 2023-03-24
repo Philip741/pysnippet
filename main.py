cd67190a66e4f602aa6d048d6a289826eb8d3961
Philip <philip741@gmail.com>
2023-03-24 14:39:04 -0400

    added store_true to the main.py menu arg

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import argparse
+import string
 import pysnip
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--menu")
+parser.add_argument("--menu", action="store_true")
 parser.add_argument("--snippets", action="store_true")
 parser.add_argument("--notes" ,action="store_true")
 parser.add_argument("--add" ,action="store_true")
-parser.add_argument("--edit")
+#using nargs can add multiple arguments and reference as a list
+parser.add_argument("--editnote", nargs=2)
+parser.add_argument("--editsnip", nargs='+')
 args = parser.parse_args()
 
 if args.menu:
@@ -19,5 +22,6 @@ elif args.notes:
     pysnip.notes_menu()
 elif args.add:
     pysnip.add_text()
-elif args.edit:
-    print(args.edit)
+elif args.editnote:
+    #pysnip.edit_cli(args.edit)
+    print(args.editnote[0])
